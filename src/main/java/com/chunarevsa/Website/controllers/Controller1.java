fb7942da26eae98f6e115c64eeeac2696baf1883
chunarevsa <chunarev.se.a@gmail.com>
2021-07-05 12:23:38 +0500

    Поиск по ID

--- src/main/java/com/chunarevsa/Website/controllers/Controller1.java
+++ src/main/java/com/chunarevsa/Website/controllers/Controller1.java
@@ -1,10 +1,24 @@
 package com.chunarevsa.Website.controllers;
 
+import java.util.ArrayList;
+import java.util.Optional;
+
+import com.chunarevsa.Website.models.Currencies;
+import com.chunarevsa.Website.models.Games;
+import com.chunarevsa.Website.models.Merchs;
+import com.chunarevsa.Website.repo.GamesRepository;
+import com.chunarevsa.Website.repo.MerchsRepository;
+import com.chunarevsa.Website.repo.СurrenciesRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
 // копируем с https://spring.io/guides/gs/serving-web-content/
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller // класс который отвечает за обработку всех переходов на сайте
@@ -23,6 +37,15 @@ public class Controller1 {
 		return "home";  вызываем определнный HTML шаблон
 	} */
 
+	@Autowired
+	private GamesRepository gamesRepository;
+
+	@Autowired
+	private MerchsRepository merchsRepository;
+
+	@Autowired
+	private СurrenciesRepository currenciesRepository;
+
 	@GetMapping("/") 
 	public String home (Model model) { 
 		model.addAttribute("title", "Главная страница"); 
@@ -35,5 +58,34 @@ public class Controller1 {
 		return "contacts"; 
 	}
 
-
+	@PostMapping ("/") 
+	public String searchId(@RequestParam long id, Model model) {
+		
+		 if (gamesRepository.existsById(id)) { 
+		  Optional<Games> game = gamesRepository.findById(id);
+		  ArrayList<Games> res = new ArrayList<>();
+		  game.ifPresent(res::add);
+		  model.addAttribute("game", res);
+		  return "games-details";
+		  } 
+		
+		  if (merchsRepository.existsById(id)) { 
+			Optional<Merchs> merch = merchsRepository.findById(id);
+			ArrayList<Merchs> res = new ArrayList<>();
+			merch.ifPresent(res::add);
+			model.addAttribute("merch", res);
+			return "merch-details";
+			} 
+		
+		  if (currenciesRepository.existsById(id)) { 
+		  Optional<Currencies> currency = currenciesRepository.findById(id);
+		  ArrayList<Currencies> res = new ArrayList<>();
+		  currency.ifPresent(res::add);
+		  model.addAttribute("currency", res);
+		  return "currencies-details";
+		  }  
+		  return "redirect:/";  
+		
+		 
+	} 
 }
