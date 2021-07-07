5ec8008132e96ae726e074c802a3cd09d751b541
chunarevsa <chunarev.se.a@gmail.com>
2021-07-07 09:50:33 +0500

    Добавление новой ветки

--- src/main/java/com/chunarevsa/Website/WebsiteApplication.java
+++ src/main/java/com/chunarevsa/Website/WebsiteApplication.java
@@ -8,10 +8,7 @@ public class WebsiteApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebsiteApplication.class, args);
-		/* Если при запуске ругается на порт 8080, то нужно зайти в
-		resources/application.properties 
-		Если в последней строке выводится Started - значит всё ок
-		Нужно обратить внимание на Tomcat - позволяет просматривать веб сайт */
+		
 		
 	} 
 
