eb8ab66be7f05c344af94007e01c8f5605505594
Chunarev_Sergey <chunarev.se.a@gmail.com>
2021-07-10 15:54:09 +0500

    controllers -  api

--- src/main/java/com/chunarevsa/Website/controllers/СurrenciesController.java
+++ src/main/java/com/chunarevsa/Website/controllers/СurrenciesController.java
@@ -6,7 +6,6 @@ import com.chunarevsa.Website.repo.СurrenciesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,8 +14,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class СurrenciesController {
 	
 	@Autowired
