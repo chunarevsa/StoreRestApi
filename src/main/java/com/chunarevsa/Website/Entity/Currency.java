052bfb570ad06ed7d2bf514152efb5a4e7bf1d76
chunarevsa <chunarev.se.a@gmail.com>
2021-08-09 12:57:35 +0500

    1

--- src/main/java/com/chunarevsa/Website/Entity/Currency.java
+++ src/main/java/com/chunarevsa/Website/Entity/Currency.java
@@ -1,17 +1,24 @@
 package com.chunarevsa.Website.Entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
 
 @Entity
+@Table(uniqueConstraints = { @UniqueConstraint(name = "UniqueCode", columnNames = { "code"}) })
 public class Currency {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-
 	private Long id; 
+
+	@Column(unique=true)
 	private String code;
+
 	private boolean active;
 
 	public Long getId() {
