643307bdd9e4f3d91d16d32fc795996e2b1d2af7
Chunarev_Sergey <chunarev.se.a@gmail.com>
2021-10-28 22:22:23 +0500

    1

--- src/main/java/com/chunarevsa/Website/Entity/User.java
+++ src/main/java/com/chunarevsa/Website/Entity/User.java
@@ -1,5 +1,59 @@
 package com.chunarevsa.Website.Entity;
 
+import java.util.Collection;
+
+import javax.persistence.Entity;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Entity
+public class User extends Base implements UserDetails {
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+}
+
 /* import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
--- src/main/java/com/chunarevsa/Website/repo/UserRepository.java
+++ src/main/java/com/chunarevsa/Website/repo/UserRepository.java
@@ -1,18 +1,12 @@
 package com.chunarevsa.Website.repo;
 
-/* import java.util.*;
-
 import com.chunarevsa.Website.Entity.User;
-
-//import org.springframework.data.domain.Page;
-//import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository <User, Long> {
 	
-	User findByUsername (String username);
+	//User findByUsername (String username);
 
-	List<User> findAll ();
-	
+	//User findByActivationCode(String code);
+
 } 
- */
